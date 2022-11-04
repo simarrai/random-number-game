b61f5ca53d150c6a4eed5ca716bffcdaa195974b
Simar <117444311+simarrai@users.noreply.github.com>
2022-11-04 12:33:21 +0000

    Difference from answer

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-import math
+import random
 #add,commit,push at each step
 num = 0 
 
 def generate_random_numbers():
-  return math.Random()
+  return random.randint(0,100)
+
+def difference_from_answer(guess,answer):
+  if guess > answer:
+    return "Too High!"
+  elif guess < answer:
+    return "Too Low!"
+  elif guess == answer:
+    return "Correct!"
+
+    
